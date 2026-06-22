969ad86e36e4bee0ed9b5fdcdc6acb25fc50943a
Nguyen Thanh Thanh <thanh.nt.set25@eiu.edu.vn>
2026-06-22 15:46:22 +0700

    Delete build directory

--- build/f1tenth_msgs/f1tenth_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /f1tenth_ws/build/f1tenth_msgs/f1tenth_msgs__py
-# Build directory: /f1tenth_ws/build/f1tenth_msgs/f1tenth_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
